6bc8bccd13d43b5330481802c7006102fef880ef
Cecilia Urbieta <142773954+ceciliaU1@users.noreply.github.com>
2024-04-18 21:27:07 +0000

    Thu, Apr 18, 2024, 2:27 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -9,7 +9,12 @@ let seconds = 0
 let score = 0
 let selected_insect = {}
 
-start_btn.addEventListener('click', () =>{
-    screens[0].classList.add
+start_btn.addEventListener('click', () => {
+    screens[0].classList.add('up')
 })
 
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img')
+    })
+})
