3e69a7904c653c4ad09dccd2138a012ac2704f79
Cecilia Urbieta <142773954+ceciliaU1@users.noreply.github.com>
2024-04-18 21:39:54 +0000

    Thu, Apr 18, 2024, 2:39 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -9,6 +9,7 @@ let seconds = 0
 let score = 0
 let selected_insect = {}
 
+console.log(screens)
 start_btn.addEventListener('click', () => {
     screens[0].classList.add('up')
 })
@@ -19,5 +20,29 @@ choose_insect_btns.forEach(btn => {
         const alt = img.getAttribute('alt')
         const src = img.getAttribute('src')
         screens[1].classList.add('up')
+        selected_insect = {src, alt}
+        setTimeout(createInsect, 1000)
+        startGame()
     })
 })
+
+
+function createInsect() {
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+    const { x, y } = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHTML = `<img src="selected_insect.src" alt="selected_insect.alt" style = "transform: rotate(${Math.random() * 360}deg />`
+
+    game_container.appedChild(insect)
+}
+
+
+function getRandomLocation() {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const x = Math.random() * (width - 200) + 100
+    const y = Math.random() * (height - 200) + 100
+    return { x, y }
+}
