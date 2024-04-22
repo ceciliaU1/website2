7d1f3dfe0f98f55c72794417bd8263e119843923
Cecilia Urbieta <142773954+ceciliaU1@users.noreply.github.com>
2024-04-22 22:00:06 +0000

    Mon, Apr 22, 2024, 3:00 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -36,7 +36,10 @@ function displayWord() {
 
 // Keydown letter press
 window.addEventListener('keydown', e => {
-    console.log(e.keyCode)
+    if (e.keyCode >= 65 && e.keyCode <=90) {
+        console.log(e.key)
+    }
 })
+
 
 displayWord()
