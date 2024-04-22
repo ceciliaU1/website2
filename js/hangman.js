a6bb3395aad2861c4033495ad1eccb4a2d4609d2
Cecilia Urbieta <142773954+ceciliaU1@users.noreply.github.com>
2024-04-22 22:06:46 +0000

    Mon, Apr 22, 2024, 3:06 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -40,13 +40,23 @@ window.addEventListener('keydown', e => {
     if (e.keyCode >= 65 && e.keyCode <=90) {
         const letter = e.key
 
-        if (selectedWord.includes(letter) {
+        if (selectedWord.includes(letter)) {
             if (!correctLetters.includes(letter)) {
                 correctLetters.push(letter)
 
                 displayWord()
+            } else {
+                showNotification()
             }
-        })
+        } else {
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter)
+
+                updateWrongLettersEl()
+            } else {
+                showNotification()
+            }
+        }
     }
 })
 
