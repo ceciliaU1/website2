edd446b8bbd48fa00942afbefb8eebc0cd2f2dde
Cecilia Urbieta <142773954+ceciliaU1@users.noreply.github.com>
2024-04-23 21:29:43 +0000

    Tue, Apr 23, 2024, 2:29 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -38,8 +38,18 @@ function displayWord() {
 function updateWrongLettersEl() {
     wrongLettersEl.innerHTML = `
         ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
-
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
     `
+
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length
+
+        if (index < errors) {
+            part.style.display = 'block'
+        } else {
+            part.style.display = 'none'
+        }
+    })
 }
 
 // Show Notification
